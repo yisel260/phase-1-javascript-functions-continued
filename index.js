50710ec4135c6705cf479e4e5f6f12447e1a3c24
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-09-25 09:59:04 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+ function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+    
+ }
+ function  mondayWork(activity2='go to the office'){
+    return `This Monday, I will ${activity2}.`
+
+ }
+
+function wrapAdjective(flair='*') {
+    return function(adjective='special') {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
